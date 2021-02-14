9282e9c02101d3274ddc026b0ada33db0027eb37
gene <galleon.eight@gmail.com>
2021-02-14 20:28:49 +0300

    Config
    Config deploy changes

--- galleon/docusaurus.config.js
+++ galleon/docusaurus.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   title: 'Welcome',
   tagline: 'to my Work Samples',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://galleon8.github.io',
+  baseUrl: '/docusaurus/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'galleon8', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
